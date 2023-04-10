7aaa85efb775d6d3d5afc6ffacd79be8f5aeec11
GaryDevOps <75078672stevenmuriel@gmail.com>
2023-04-09 22:26:57 -0500

    Styles for Login and validations, :)

--- src/components/InitialPage/InitialPage.jsx
+++ src/components/InitialPage/InitialPage.jsx
@@ -1,7 +1,48 @@
 import style from './InitialPage.module.css'
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 function InitialPage() {
+
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleOnChange = (event) => { // Recibo cada caracter de los input
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    });
+    validate();
+  };
+  
+  const navigate = useNavigate()
+  const handleOnSubmit = (event) => {
+    const emailRoot = '1234@gmail.com';
+    const passowrdRoot = '1234';
+    if (form.email === emailRoot && form.password === passowrdRoot) {
+      navigate('/home')
+    }
+    else alert('Correo invalido');
+    event.preventDefault();
+  };
+
+  const validate = () => {
+    if (!/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(form.email)) {
+      setErrors({
+        ...errors,
+        email: 'Por favor, revisa tu email, rey/reina'
+      });
+    }
+    else setErrors({...errors, email: ''})
+  };
+
   return (
     <div className={style.body}>
       <div className={style.half}>
@@ -11,22 +52,23 @@ function InitialPage() {
 
             <div className={`${style.form_box} ${style.login}`}>
               <h2>Login</h2>
-              <form action="#">
+              <form onSubmit={handleOnSubmit}>
                 <div className={style.input_box}>
                   <span className={style.icon}><img className={style.img} src={require('../../img/email.png')} alt='Icono de email'/></span>
-                  <input type="email" required/>
+                  <input name="email" type="text" value={form.email} onChange={handleOnChange} required/>
                   <label>Email</label>
+                  {errors.email && <p className={style.p}>{errors.email}</p>}
                 </div>
                 <div className={style.input_box}>
                   <span className={style.icon}><img className={style.img} src={require('../../img/lock.png')} alt='Icono de passowrd'/></span>
-                  <input type="password" required/>
+                  <input name="password" type="password" value={form.password} onChange={handleOnChange} required/>
                   <label>Password</label>
                 </div>
                 <div className={style.remember_forgot}>
                   <label><input type='checkbox'></input> Remember me</label>
                   <NavLink to="#">Forgot Password?</NavLink>
                 </div>
-                <button type='submit' className={style.btn}>Login</button>
+                <button type='submit' className={style.btn} disabled={!form.email || !form.password || errors.email} >Login</button>
                 <div className={style.login_register}>
                   <p>Don't have an account? <NavLink to="#" className={style.register_link}>Register</NavLink></p>
                 </div>
@@ -61,8 +103,7 @@ function InitialPage() {
               </form>
             </div> */}
 
-          </div>
-        <NavLink to='/home'>Iniciar Sesion</NavLink>  
+          </div> 
         </div>
       </div>
     </div>
